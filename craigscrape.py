d214a679e6800c2aab4bad05b7ba3cc6eb25798c
Deen Islam <muslimbi@users.noreply.github.com>
2022-01-17 10:08:17 +0600

    updated 20220117

--- craigscrape.py
+++ craigscrape.py
@@ -11,28 +11,31 @@ from datetime import datetime
 from random import randint
 import base64
 
+
 class Search(object):
-    def __init__ (self, loc, year, make, model, key):
+    def __init__(self, loc, year, make, model, key):
         self.loc = loc
         self.year = year
         self.make = make
         self.model = model
         self.key = key
 
-#potentially take user info from CL if client wants to run script without interacting with code
-scrape_list = ['bham_2001_ford_ranger'] #loc_year_make_model, append similar format for other vehicles
 
-#holds posts that have been accounted for, keyed by the search terms
+# potentially take user info from CL if client wants to run script without interacting with code
+scrape_list = ['bham_2001_ford_ranger']  # loc_year_make_model, append similar format for other vehicles
+
+# holds posts that have been accounted for, keyed by the search terms
 ref_dict = {}
 for i in scrape_list:
     ref_dict[i] = []
 
-#https://bham.craigslist.org/search/cta?query=2002+toyota+highlander&auto_paint=9
+
+# https://bham.craigslist.org/search/cta?query=2002+toyota+highlander&auto_paint=9
 def heavy_scrape(target):
     print('\n[+++   SCRAPER ACTIVATED', datetime.now(), '  +++]')
 
     try:
-        result = get_req(target) #result is list of html segments contianing each post
+        result = get_req(target)  # result is list of html segments contianing each post
         for t in result:
             line = str(t).split("<")
             pid = ''
@@ -44,29 +47,29 @@ def heavy_scrape(target):
             for j in line:
                 if "data-pid" in j and "maptag" not in j:
                     pid_frame = j.split('">')
-                    inter = pid_frame[0].replace('li class="result-row" data-pid="','')
+                    inter = pid_frame[0].replace('li class="result-row" data-pid="', '')
                     pid = inter.split('"')[0]
                 elif "result-price" in j:
-                    price = j.replace('span class="result-price">','')
+                    price = j.replace('span class="result-price">', '')
                 elif "datetime=" in j:
                     date_frame = j.split('title=')
                     date2_frame = date_frame[1].split('>')
-                    post_date = date2_frame[0].replace('"','')
+                    post_date = date2_frame[0].replace('"', '')
                 elif 'class="result-hood"' in j:
                     loc_frame = j.split('(')
                     location = loc_frame[1].replace(')', '')
                 elif 'class="nearby"' in j:
                     loc2_frame = j.split('>')
-                    location = loc2_frame[0].replace('span class="nearby" title=','').replace('"','')
+                    location = loc2_frame[0].replace('span class="nearby" title=', '').replace('"', '')
                 elif 'result-title hdrlnk' in j:
                     url_frame = j.split('href=')
                     url2_frame = url_frame[1].split('>')
                     description = url2_frame[1]
-                    link = url2_frame[0].replace('"','')
+                    link = url2_frame[0].replace('"', '')
                     if 'craigslist' not in link:
                         link = 'https://' + loc + '.craigslist.org' + link
-            
-            if target.model.upper() in description.upper() and target.year in description: #keeps "similar" results out
+
+            if target.model.upper() in description.upper() and target.year in description:  # keeps "similar" results out
                 if pid not in ref_dict[target.key]:
                     ref_dict[target.key].append(pid)
                     print("[+]     *** NEW POSTING ***    *** NEW POSTING ***     [+]")
@@ -76,9 +79,9 @@ def heavy_scrape(target):
                     print('Price:', price)
                     print('PID:', pid)
                     print('Link:', link, '\n')
-                    
+
                     text_alert(post_date, location, year, model, price, link)
-            
+
     except:
         print("[-] HEAVY SCRAPER CRASH [-]")
 
@@ -94,70 +97,70 @@ def light_scrape(target):
             for j in line:
                 if "data-pid" in j and "maptag" not in j:
                     pid_frame = j.split('">')
-                    inter = pid_frame[0].replace('li class="result-row" data-pid="','')
+                    inter = pid_frame[0].replace('li class="result-row" data-pid="', '')
                     pid = inter.split('"')[0]
                 elif 'result-title hdrlnk' in j:
                     url_frame = j.split('href=')
                     url2_frame = url_frame[1].split('>')
-                    description = url2_frame[1] 
-                
-            if target.model.upper() in description.upper() and target.year in description: #keeps irrelevant results out
+                    description = url2_frame[1]
+
+            if target.model.upper() in description.upper() and target.year in description:  # keeps irrelevant results out
                 if pid not in ref_dict[target.key]:
                     ref_dict[target.key].append(pid)
                     print("[+]                *** PID LIST UPDATED ***                [+]")
                     print("                 ~ NO EXTERNAL NOTIFICATION ~")
-
-                    
     except:
         print("[-] LIGHT SCRAPER CRASH [-]")
 
+
 def get_req(target):
-    #HTTP Requests
+    # HTTP Requests
     url = 'https://' + target.loc + '.craigslist.org/search/cta?query=' + target.year + '+' + target.model + '+' + target.make
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    r = http.request('GET', url, headers={'User-agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'})
+    r = http.request('GET', url, headers={
+        'User-agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'})
 
-    #Parsing
+    # Parsing
     html = r.data
     soup = bs.BeautifulSoup(html, 'lxml')
-    result = soup.find_all('li', {'class':"result-row"}) #separates each post
+    result = soup.find_all('li', {'class': "result-row"})  # separates each post
     return result
 
 
 def text_alert(post_date, location, year, model, price, link):
-    #Texting authorization
+    # Texting authorization
     from twilio.rest import Client
     creds = []
-    cred_file = open('k:/Creds/twilio_creds.txt','r')
-    #decrypts the saved file containing access tokens
+    cred_file = open('k:/Creds/twilio_creds.txt', 'r')
+    # decrypts the saved file containing access tokens
     for entry in cred_file.readlines():
         i = base64.b64decode(entry.strip('\n'))
-        creds.append(str(i,'utf-8'))
+        creds.append(str(i, 'utf-8'))
 
     ACCOUNT_SID = creds[0]
-    AUTH_TOKEN  = creds[1]
+    AUTH_TOKEN = creds[1]
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
     message = post_date + ' in ' + location + ', ' + year + ' ' + model + ' for ' + price + ', Link: ' + link
     try:
         client.messages.create(
-        to= "", #creds[2], #"1"+"9DIGIT#"
-        from_=creds[3],
-        body=message
+            to="",  # creds[2], #"1"+"9DIGIT#"
+            from_=creds[3],
+            body=message
         )
         print("[+] Text Alert Sent [+]")
     except:
         print("[-] !!! Text Alert Failure !!! [-]")
 
-    
+
 def main():
-    #read_in() #reads in all catalogued posts, and adds them to instance memory so that text results arent sent out for old vehicles
+    # read_in() #reads in all catalogued posts, and adds them to instance memory so that text results arent sent out for old vehicles
     target_list = []
     try:
         for i in scrape_list:
             beta = i.split('_')
             target = Search(beta[0], beta[1], beta[2], beta[3], i)
-            target_list.append(target) #stores target objects
+            target_list.append(target)  # stores target objects
             light_scrape(target)
     except KeyboardInterrupt:
         print("[-] Manually Exited During Startup [-]")
@@ -167,9 +170,10 @@ def main():
         try:
             for entry in target_list:
                 heavy_scrape(entry)
-                time.sleep(randint(5, 9)) #feeble attempt to not look like a bot when iterating over several target vehicles
+                time.sleep(
+                    randint(5, 9))  # feeble attempt to not look like a bot when iterating over several target vehicles
 
-            time.sleep(120) #sleeps 2 min after iterating over all the cars were looking for
+            time.sleep(120)  # sleeps 2 min after iterating over all the cars were looking for
         except KeyboardInterrupt:
             print("[-] Manual Exit [-]")
             run = False
@@ -179,4 +183,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
--- craigslistScraper.py
+++ craigslistScraper.py
@@ -22,164 +22,165 @@ prev_matches = [['']]
 
 
 def getPage(url):
-	try:
-		with closing(get(url, stream=True)) as resp:
-			if isGoodResp(resp):
-				return resp.content
-			else:
-				return None
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if isGoodResp(resp):
+                return resp.content
+            else:
+                return None
 
-	except RequestException as e:
-		log_error('Error during request to {0} : {1}'.format(url, str(e)))
-		return None
+    except RequestException as e:
+        log_error('Error during request to {0} : {1}'.format(url, str(e)))
+        return None
 
 
 def isGoodResp(resp):
-	content_type = resp.headers['Content-Type'].lower()
-	return (resp.status_code == 200
-			and content_type is not None
-			and content_type.find('html') > -1)
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
 
 
 def log_error(e):
-	fid = open('log.txt', 'w')
-	fid.write(e)
-	fid.close()
+    fid = open('log.txt', 'w')
+    fid.write(e)
+    fid.close()
 
 
 def fileDump(content):
-	fid = open('pageData.txt', 'w')
-	fid.write(content)
-	fid.close()
+    fid = open('pageData.txt', 'w')
+    fid.write(content)
+    fid.close()
 
 
-def printTitles(results): # Prints only the titles of a results list
-	N = len(results)
+def printTitles(results):  # Prints only the titles of a results list
+    N = len(results)
 
-	if N == 0: return
+    if N == 0: return
 
-	for i in range(0,N):
-		string = str(i) + ': ' + results[i][0]
-		print(string)
+    for i in range(0, N):
+        string = str(i) + ': ' + results[i][0]
+        print(string)
 
 
 def getResults(content):
-	html = BS(content, 'html.parser')
-	p = html.select('p')
-	results = []
+    html = BS(content, 'html.parser')
+    p = html.select('p')
+    results = []
 
-	for i in range(0, len(p)):
-		if str(p[i]).find('result-info'): #Found the beginning of a result section
-			result_contents = [None]*4 # Title, Date, Neighborhood, Link			
-			line = str(p[i]).split('\n') # Break the result section up by lines
-			
-			for j in range(0,len(line)):
-				if line[j].find('result-hood') > -1: # Found the neighborhood data
-					ind1 = line[j].find('(')+1
-					ind2 = line[j].find(')')
-					result_contents[2] = line[j][ind1:ind2] # Isolate neighborhood
+    for i in range(0, len(p)):
+        if str(p[i]).find('result-info'):  # Found the beginning of a result section
+            result_contents = [None] * 4  # Title, Date, Neighborhood, Link
+            line = str(p[i]).split('\n')  # Break the result section up by lines
 
-				if line[j].find('result-date') > -1: # Found post time info
-					ind1 = line[j].find('title=')+7
-					ind2 = line[j].find('>')-1
-					result_contents[1] = line[j][ind1:ind2] # Isolate date and time
+            for j in range(0, len(line)):
+                if line[j].find('result-hood') > -1:  # Found the neighborhood data
+                    ind1 = line[j].find('(') + 1
+                    ind2 = line[j].find(')')
+                    result_contents[2] = line[j][ind1:ind2]  # Isolate neighborhood
 
-				if line[j].find('result-title hdrlnk') > -1: # Found result title and link
-					ind1 = line[j].find('>')+1
-					ind2 = line[j].find('</a>')
-					result_contents[0] = line[j][ind1:ind2] # Isolate title
+                if line[j].find('result-date') > -1:  # Found post time info
+                    ind1 = line[j].find('title=') + 7
+                    ind2 = line[j].find('>') - 1
+                    result_contents[1] = line[j][ind1:ind2]  # Isolate date and time
 
-					ind1 = line[j].find('href=')+6
-					ind2 = line[j].find('>')-1
-					result_contents[3] = line[j][ind1:ind2] # Isolate link
+                if line[j].find('result-title hdrlnk') > -1:  # Found result title and link
+                    ind1 = line[j].find('>') + 1
+                    ind2 = line[j].find('</a>')
+                    result_contents[0] = line[j][ind1:ind2]  # Isolate title
 
-			results.append(result_contents)
-			
-	return results
+                    ind1 = line[j].find('href=') + 6
+                    ind2 = line[j].find('>') - 1
+                    result_contents[3] = line[j][ind1:ind2]  # Isolate link
+
+            results.append(result_contents)
+
+    return results
 
 
 def searchResults(results, keywords):
-	n_res = len(results)
-	n_keys = len(keywords)
-	matching_results = []
+    n_res = len(results)
+    n_keys = len(keywords)
+    matching_results = []
 
-	for i in range(0, n_res):
-		for j in range(0, n_keys):
-			if results[i][0].upper().find(keywords[j].upper()) > -1: # Found a matching result
-				matching_results.append(results[i])
+    for i in range(0, n_res):
+        for j in range(0, n_keys):
+            if results[i][0].upper().find(keywords[j].upper()) > -1:  # Found a matching result
+                matching_results.append(results[i])
 
-	return matching_results
+    return matching_results
 
 
 def manageResults(new_matches):
-	global prev_matches
-	identical = []
+    global prev_matches
+    identical = []
 
-	print('\n\n============================================')
-	print('\nPREVIOUS MATCHES\n')
-	printTitles(prev_matches)
-	print('\nNEW MATCHES\n')
-	printTitles(new_matches)
+    print('\n\n============================================')
+    print('\nPREVIOUS MATCHES\n')
+    printTitles(prev_matches)
+    print('\nNEW MATCHES\n')
+    printTitles(new_matches)
 
-	# Compare the new and previous matching results
-	for i in range(0,len(prev_matches)):
-		for j in range(0,len(new_matches)):
-			if prev_matches[i] == new_matches[j]: # found an identical result. Record the new_matches index so we can remove it
-				identical.append(new_matches[j])
+    # Compare the new and previous matching results
+    for i in range(0, len(prev_matches)):
+        for j in range(0, len(new_matches)):
+            if prev_matches[i] == new_matches[
+                j]:  # found an identical result. Record the new_matches index so we can remove it
+                identical.append(new_matches[j])
 
-	print('\nIDENTICAL MATCHES\n')
-	printTitles(identical)
-	# Remove the identical results
-	for i in range(0,len(identical)):
-		try:
-			new_matches.remove(identical[i])
-		except:
-			print('result error')
+    print('\nIDENTICAL MATCHES\n')
+    printTitles(identical)
+    # Remove the identical results
+    for i in range(0, len(identical)):
+        try:
+            new_matches.remove(identical[i])
+        except:
+            print('result error')
 
-	# Add the remaining non-matching results to previous results
-	for i in range(0,len(new_matches)):
-		prev_matches.append(new_matches[i])
+    # Add the remaining non-matching results to previous results
+    for i in range(0, len(new_matches)):
+        prev_matches.append(new_matches[i])
 
-	print('\nUNIQUE MATCHES\n')
-	printTitles(new_matches)
+    print('\nUNIQUE MATCHES\n')
+    printTitles(new_matches)
 
-	return new_matches
+    return new_matches
 
 
 def getAccounts():
-	global addressTo
-	global addressFrom
-	global password
+    global addressTo
+    global addressFrom
+    global password
 
-	#addressTo = input("Enter the email address to receive notifications: ")
-	#addressFrom = input("Enter the email address to send notifications from: ")
-	#password = getpass.getpass(prompt = 'Enter the sending account password: ')
+    # addressTo = input("Enter the email address to receive notifications: ")
+    # addressFrom = input("Enter the email address to send notifications from: ")
+    # password = getpass.getpass(prompt = 'Enter the sending account password: ')
 
-	# For development use
-	addressTo = 'tyler.slone47@gmail.com'
-	addressFrom = 'tyler.python.email@gmail.com'
-	password = 'txS485FI0WI&'
+    # For development use
+    addressTo = 'tyler.slone47@gmail.com'
+    addressFrom = 'tyler.python.email@gmail.com'
+    password = 'txS485FI0WI&'
 
 
 def getKeywords():
-	global keywords
+    global keywords
 
-	userStr = input('Enter search keywords separated by commas: ')
-	userStr = userStr.split(',')
+    userStr = input('Enter search keywords separated by commas: ')
+    userStr = userStr.split(',')
 
-	for i in range(0,len(userStr)):
-		if userStr[i][0] == ' ': # The first character is a space. Remove the 1st character.
-			userStr[i] = userStr[i][1:len(userStr[i])]
+    for i in range(0, len(userStr)):
+        if userStr[i][0] == ' ':  # The first character is a space. Remove the 1st character.
+            userStr[i] = userStr[i][1:len(userStr[i])]
 
-		keywords.append(userStr[i])
+        keywords.append(userStr[i])
 
 
 def makeEmail(match):
-	header = '''From: Craigslist Scraper <%s>
+    header = '''From: Craigslist Scraper <%s>
 
 	''' % addressFrom
 
-	contents = '''Craigslist Scraper has identified a new result that matches your keywords: %s
+    contents = '''Craigslist Scraper has identified a new result that matches your keywords: %s
 
 	Title: %s
 	Posting Time: %s
@@ -188,57 +189,57 @@ def makeEmail(match):
 
 	''' % (str(keywords).strip('[').strip(']'), match[0], match[1], match[2], match[3])
 
-	email = header + contents
+    email = header + contents
 
-	print(email)
+    print(email)
 
-	return email
+    return email
 
 
 def sendEmail(addressFrom, addressTo, password, contents, port):
-	server = 'smtp.gmail.com'
+    server = 'smtp.gmail.com'
 
-	try:
-		server = smtplib.SMTP(server, port)
-		server.ehlo()
-		server.starttls()
-		server.ehlo()
-		server.login(addressFrom, password)
-		
-		server.sendmail(addressFrom, addressTo, contents)
+    try:
+        server = smtplib.SMTP(server, port)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(addressFrom, password)
 
-	except Exception as e:
-		print(e)
-	finally:
-		server.quit()
+        server.sendmail(addressFrom, addressTo, contents)
+
+    except Exception as e:
+        print(e)
+    finally:
+        server.quit()
 
 
 def report(matches):
-	global addressFrom
-	global addressTo
-	global password
-	global port
+    global addressFrom
+    global addressTo
+    global password
+    global port
 
-	# Iterate through the matching results and send an email for each one
-	N = len(matches)
+    # Iterate through the matching results and send an email for each one
+    N = len(matches)
 
-	for i in range(0,N):
-		email_body = makeEmail(matches[i])
-		sendEmail(addressFrom, addressTo, password, email_body, port)
+    for i in range(0, N):
+        email_body = makeEmail(matches[i])
+        sendEmail(addressFrom, addressTo, password, email_body, port)
 
 
 def periodicScrape(period):
-	getAccounts()
-	getKeywords()
+    getAccounts()
+    getKeywords()
 
-	while(True):
-		content = getPage(url)
-		results = getResults(content)
-		new_matches = searchResults(results, keywords)
-		unique_matches = manageResults(new_matches)
-		report(unique_matches)
+    while (True):
+        content = getPage(url)
+        results = getResults(content)
+        new_matches = searchResults(results, keywords)
+        unique_matches = manageResults(new_matches)
+        report(unique_matches)
 
-		time.sleep(period)
+        time.sleep(period)
 
 
 periodicScrape(60)
